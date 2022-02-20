6351e10f5f4af30be78bcf50c9047a66ba9c1b8e
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-02-20 18:29:31 +0200

    database make migration

--- database/migrations/2022_02_20_155136_create_posts_table.php
+++ database/migrations/2022_02_20_155136_create_posts_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->text('Title', 100);
+            $table->text('Posted By', 50);
+            $table->text('description', 200);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('posts');
     }
 };
