41f514ffdc2f8e0127593a2a4571ab87e9d9a38c
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-02-21 09:20:33 +0200

    factory created for users and posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $postsArr = Post::paginate(2);
+        $postsArr = Post::paginate(10);
         // dd($postsArr);
 
         return view('posts.index', ['posts'=>$postsArr]);
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,8 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Post;
-
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -19,11 +17,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-           'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'title' => $this->faker->text(50),
-            'description' => $this->faker->paragraph(),
-            'user_id' => Post::factory(),
+            'title' => $this->faker->text,
+            'description' => $this->faker->text
+
         ];
     }
 }
